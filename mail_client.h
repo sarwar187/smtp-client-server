2b2c4654d1df941d1a1896ee2b89fa2cbde33811
sarwar187 <sarwar187@yahoo.com>
2013-07-08 01:58:48 +0600

    Create mail_client.h

---
+++ mail_client.h
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <string.h>
+#define TRUE 1
