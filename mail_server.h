33bd4489f8d4e99989dd08e60e0af17b519823a0
sarwar187 <sarwar187@yahoo.com>
2013-07-08 01:59:56 +0600

    Create mail_server.h

---
+++ mail_server.h
@@ -0,0 +1,12 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <string.h>
+#include<ctype.h>
+#define TRUE 1
